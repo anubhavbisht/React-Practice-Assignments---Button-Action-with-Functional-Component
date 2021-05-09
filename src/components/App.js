5a5a9218d6a3c5d6613c2a958258fe38d93b6953
Anubhav Bisht <40894333+anubhavbisht@users.noreply.github.com>
2021-05-09 22:27:37 +0530

    useState hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [state, changestate] = useState(false);
+  const parashow = () => {
+    changestate(true);
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={parashow}>
+        Click Me To Show Para
+      </button>
+      <div>
+        {state ? (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        ) : null}
+      </div>
     </div>
   );
 }
-
 
 export default App;
